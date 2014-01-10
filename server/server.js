61e1505c93b0c2d070a078a1544f970ff630e82e
marcarudolph <marc.a.rudolph@gmail.com>
2014-01-10 12:48:56 +0100

    Support underscore character in channelname as a substitute for blanks

--- server/server.js
+++ server/server.js
@@ -165,8 +165,9 @@ function initChannelInfosForAllDevices(continueWith) {
 
 function initServer() {
 
-    app.get("/streams/:channelName", function(request, response) {        
-        tuneChannel(request.params.channelName, function (result) {
+    app.get("/streams/:channelName", function(request, response) {
+        var channelName = request.params.channelName.replace('_', ' ');
+        tuneChannel(channelName, function (result) {
             if (result.success) {
 
                 var streamUrl = 'http://' + result.host + ':' + result.streamingPort;
